a5173cb8af893d57955438a9dd23d190278cb6c2
Igor Rocha <irocha@magnasistemas.com.br>
2023-04-20 13:26:12 -0300

    Melhorando a documentação da API

--- src/main/java/br/com/magna/animal/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/br/com/magna/animal/api/infra/springdoc/SpringDocConfigurations.java
@@ -19,8 +19,13 @@ public class SpringDocConfigurations {
 				.components(new Components().addSecuritySchemes("bearer-key",
 						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
 				.info(new Info().title("Magna Zoo").description(
-						"API Rest da aplicação Magna Zoo. No momento, contém apenas a funcionalidade de CRUD de animais mamíferos. " + 
-				"Posteriormente, pretendemos adicionar essa funcionalidade para outros tipos de animais, como peixes, aves, répteis...")
+						"Essa API Rest com Spring Boot permite realizar operações CRUD (Create, Read, Update, Delete) de mamíferos. "
+						+ "Através dela, é possível criar, recuperar, atualizar e excluir registros de mamíferos armazenados em um banco de dados. "
+						+ "\nOs dados que podem ser inseridos na criação de um registro incluem o nome do mamífero, sua cor, peso, se ele é vertebrado ou invertebrado, "
+						+ "o tipo de sangue, se possui pelos, a presença de glândulas mamárias, a quantidade de patas, o tipo do mamífero e sua alimentação e ambiente."
+						+ "\nA API é construída seguindo os princípios do protocolo HTTP, tornando-se fácil de integrar com outras aplicações e sistemas. "
+						+ "Ela oferece respostas em formato JSON, que é amplamente utilizado no desenvolvimento de aplicações web. Além disso, a API possui um "
+						+ "conjunto de endpoints bem definidos para cada operação CRUD.")
 						.contact(new Contact().name("Magna Zoo - Igor").email("irocha@magnasistemas.com").url("https://github.com/IgorTeixeira01"))
 						.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html"))
 						.version("1.0.1"));
