efe19b4155af15c6c99502517c9866f4691ad0dd
Igor Rocha <irocha@magnasistemas.com.br>
2023-05-10 08:59:37 -0300

    Organizando pastas e ajustando documentação

--- src/main/java/br/com/magna/animal/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/br/com/magna/animal/api/infra/springdoc/SpringDocConfigurations.java
@@ -26,7 +26,7 @@ public class SpringDocConfigurations {
 						+ "\nA API é construída seguindo os princípios do protocolo HTTP, tornando-se fácil de integrar com outras aplicações e sistemas. "
 						+ "Ela oferece respostas em formato JSON, que é amplamente utilizado no desenvolvimento de aplicações web. Além disso, a API possui um "
 						+ "conjunto de endpoints bem definidos para cada operação CRUD.")
-						.contact(new Contact().name("Magna Zoo - Igor").email("irocha@magnasistemas.com").url("https://github.com/IgorTeixeira01"))
+						.contact(new Contact().name("Magna Zoo - Igor").email("irocha@magnasistemas.com").url("http://git.magnasistemas.com.br/irocha/api_mamiferos"))
 						.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html"))
 						.version("1.0.1"));
 	}
